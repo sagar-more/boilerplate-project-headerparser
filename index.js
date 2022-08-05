83f9dbe359b331efa576d2cc5bbd90359b857081
sagar-more <33574366+sagar-more@users.noreply.github.com>
2022-08-05 19:24:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const data = {
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  }
+  res.json(data);
 });
 
 // listen for requests :)
